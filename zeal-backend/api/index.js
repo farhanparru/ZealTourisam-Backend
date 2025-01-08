b1894f424bf014648eaa258c1f3d86a35b8e24f3
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-01-08 10:48:15 -0500

    message

--- zeal-backend/api/index.js
+++ zeal-backend/api/index.js
@@ -50,6 +50,12 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+app.get('/',(req,res)=>{
+    res.send('MY ZealTourisam')
+})
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
