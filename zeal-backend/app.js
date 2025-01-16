bbcb47a8513cf1fa88aee932ef99b1aa38183b5f
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-01-15 21:18:43 -0500

    updated

--- zeal-backend/app.js
+++ zeal-backend/app.js
@@ -1,50 +1,41 @@
 require("dotenv").config()
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');   
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
-var indexRouter = require('./routes/index');
+var cors = require("cors")
+const { default: mongoose } = require("mongoose");
+const bodyParser = require("body-parser");
+
+
 
 var app = express();
 
-app.use(cors())
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
-
-app.use('/', indexRouter);
 app.use('/api/holidays', require("./routes/holidays/index"));
 app.use('/api/admin', require("./routes/admin/index"));
 app.use('/api/global-visa', require("./routes/global-visa/index"));
 app.use('/api/umrahaall', require('./routes/Umraha for all/index'))
-app.use('/api/Enquiryss', require('./routes/EnquiryVisa/index'))
+app.use('/api/enquire', require("./routes/enquire/index"))
+app.use('/api/globalvisa', require('./routes/EnquiryVisa/index'))
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Middleware to parse JSON bodies
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to ZealTourism Backend!');
 });
 
 
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
