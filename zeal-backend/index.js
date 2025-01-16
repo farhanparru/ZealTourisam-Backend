59b3afebf0fbeedeccec268f047cf1d4a49125c7
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-01-15 20:49:00 -0500

    corccte update

--- zeal-backend/index.js
+++ zeal-backend/index.js
@@ -12,9 +12,9 @@ const { default: mongoose } = require("mongoose");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 app.use(cors())
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 
-// app.use('/api/model', require("./routes/model/index"))
+
 app.use('/', indexRouter);
 app.use('/api/holidays', require("./routes/holidays/index"));
 app.use('/api/admin', require("./routes/admin/index"));
@@ -31,19 +31,8 @@ app.use('/api/umrahaall', require('./routes/Umraha for all/index'))
 app.use('/api/enquire', require("./routes/enquire/index"))
 app.use('/api/globalvisa', require('./routes/EnquiryVisa/index'))
 
-// Middleware to parse JSON bodies
-// app.use(bodyParser.json());
 
-// Endpoint to handle enquiry submission
-app.post('/api/enquiry', (req, res) => {
-  const enquiryData = req.body;
-  console.log('Received Enquiry Data:', enquiryData);
 
-  // Here you can handle the data (save to database, send email, etc.)
-
-  // Respond back to the client
-  res.json({ message: 'Enquiry received successfully!' });
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
