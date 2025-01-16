219f96dd8a2871537892430f2f369c7c44c97673
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-01-15 21:08:12 -0500

    final update to vercel

--- zeal-backend/app.js
+++ zeal-backend/app.js
@@ -2,19 +2,18 @@ require("dotenv").config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');   
 var logger = require('morgan');
-var cors = require("cors")
+const cors = require('cors');
 var indexRouter = require('./routes/index');
-const { default: mongoose } = require("mongoose");
-
 
 var app = express();
 
-// view engine setup
-
 app.use(cors())
-app.use(bodyParser.json());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,17 +21,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 
-
 app.use('/', indexRouter);
 app.use('/api/holidays', require("./routes/holidays/index"));
 app.use('/api/admin', require("./routes/admin/index"));
 app.use('/api/global-visa', require("./routes/global-visa/index"));
 app.use('/api/umrahaall', require('./routes/Umraha for all/index'))
-app.use('/api/enquire', require("./routes/enquire/index"))
-app.use('/api/globalvisa', require('./routes/EnquiryVisa/index'))
-
-
-
+app.use('/api/Enquiryss', require('./routes/EnquiryVisa/index'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,9 +34,7 @@ app.use(function(req, res, next) {
 });
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to ZealTourism Backend!');
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
