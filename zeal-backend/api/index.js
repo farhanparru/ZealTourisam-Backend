0e2a8cb1aee74c5948bff9f274fdb695f8f02df7
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-06-10 23:17:11 -0500

    main file change

--- zeal-backend/api/index.js
+++ zeal-backend/api/index.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve static files from uploads directory
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));// Make sure this matches your BASE_U
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));// Make sure this matches your BASE_U
 console.log("Uploads path:", path.join(__dirname, "uploads"));
 
 
