1f0951e8b8e310c5d16d540917665d8014d5638f
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-06-10 23:37:11 -0500

    update

--- zeal-backend/routes/admin/index.js
+++ zeal-backend/routes/admin/index.js
@@ -6,7 +6,6 @@ const adminAuth = require('../../middlewares/AdminAuth'); // Adjust path as need
 // Login an admin
 router.post('/login', adminController.login);
 
-// Protected route
 router.get('/protected', adminAuth, adminController.protected);
 
 router.post('/adminLogout', adminController.AdminLogout)
